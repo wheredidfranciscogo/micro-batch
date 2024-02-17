4d72958fc41f097fc2d512d947e393c12b23b49d
Francisco Arrieta <61140677+wheredidfranciscogo@users.noreply.github.com>
2024-02-17 21:09:12 +1100

    feat: add robot classes and meet requirments

--- batching.js
+++ batching.js
@@ -12,6 +12,21 @@ Class Job:
 
 Class JobResult:
     - carry the result data / error of the job
+
+    Define the classes for the Robot, RobotComponent and RobotBuilder
+
+Class Robot:
+    - components
+    - number / serial number
+
+Class RobotComponent
+    - name
+
+Class RobotBuilder
+    - batch size
+    - build time frequency
+    - handle batch completion
+
 */
 
 class Job {
@@ -26,3 +41,26 @@ class JobResult {
     this.error;
   }
 }
+
+class Robot {
+  constructor(components) {
+    this.components;
+  }
+}
+
+class RobotComponent {
+  constructor(name) {
+    this.name;
+  }
+}
+
+class RobotBuilder {
+  // This class has to handle the batchSize, the building time and control when the batch (Robot) is complete
+  constructor(batchSize, buildTime, handleBatchCompletion) {
+    this.batchSize;
+    this.buildTime;
+    this.handleBatchCompletion;
+  }
+}
+
+
